508173b382675c2f3b760c0cd8fdad8329beded8
KristofferGW <kristoffer.wallqvist@gmail.com>
2025-02-24 12:25:19 +0100

    Added StudentName component

---
+++ src/app/components/StudentName.js
@@ -0,0 +1,10 @@
+const StudentName = ({ name }) => {
+    return (
+      <div className="w-[636px] h-20 bg-gray-200 shadow-md flex items-center justify-center text-xl font-semibold text-gray-800 mx-auto">
+        {name}
+      </div>
+    );
+  };
+  
+  export default StudentName;
+  
