ac8554f627b91d8c03419417caa36d2fe8ec95de
KristofferGW <kristoffer.wallqvist@gmail.com>
2025-02-24 12:02:50 +0100

    default function changed from RootLayout to Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 
-export default function RootLayout({ children }) {
+// export default function RootLayout({ children }) {
+export default function Page({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
