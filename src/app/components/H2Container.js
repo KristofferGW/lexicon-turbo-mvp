9fafc7f81b2226ce9f587194b5ed682371c5a643
KristofferGW <kristoffer.wallqvist@gmail.com>
2025-02-27 20:35:00 +0100

    Changed bg color of h2container and studentname to white

--- src/app/components/H2Container.js
+++ src/app/components/H2Container.js
@@ -1,6 +1,6 @@
 export default function H2Container({ headline }) {
     return (
-      <div className="w-[636px] bg-gray-200 border-t border-lexicon shadow-md flex items-center justify-center text-l font-semibold text-gray-800 mx-auto py-3">
+      <div className="w-[636px] bg-white border-t border-lexicon shadow-md flex items-center justify-center text-l font-semibold text-gray-800 mx-auto py-3">
         {headline}
       </div>
     );
--- src/app/components/StudentName.js
+++ src/app/components/StudentName.js
@@ -1,6 +1,6 @@
 export default function StudentName({ name }) {
     return (
-      <div className="w-[636px] bg-gray-200 shadow-md flex items-center justify-center text-xl font-semibold text-gray-800 mx-auto py-3">
+      <div className="w-[636px] bg-white shadow-md flex items-center justify-center text-xl font-semibold text-gray-800 mx-auto py-3">
         {name}
       </div>
     );
