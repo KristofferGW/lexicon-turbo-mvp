123c55dc4fb28f131deab0eac16406a56a0c3eb5
KristofferGW <kristoffer.wallqvist@gmail.com>
2025-02-27 20:24:27 +0100

    Fixed text color in header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,19 +4,19 @@ const Header = () => {
   return (
     <header className="w-full bg-lexicon shadow-sm border">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 h-16 flex items-center justify-between">
-        <Link href="/" className="font-semibold text-xl text-white-800">
+        <Link href="/" className="font-semibold text-xl text-white">
           LEXIKON TURBO
         </Link>
         
         <nav>
           <ul className="flex space-x-6">
             <li>
-              <Link href="/settings" className="text-white-600 hover:text-white-900">
+              <Link href="/settings" className="text-white hover:text-gray-200">
                 Settings
               </Link>
             </li>
             <li>
-              <Link href="/info" className="text-white-600 hover:text-white-900">
+              <Link href="/info" className="text-white hover:text-gray-200">
                 Info
               </Link>
             </li>
