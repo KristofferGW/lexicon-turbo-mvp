7872c938c243cbf82cc998f552cae3a064820e68
KristofferGW <kristoffer.wallqvist@gmail.com>
2025-02-27 20:30:07 +0100

    Added top and bottom padding to H2Container StudentName component

--- src/app/components/StudentName.js
+++ src/app/components/StudentName.js
@@ -1,6 +1,6 @@
 export default function StudentName({ name }) {
     return (
-      <div className="w-[636px] h-20 bg-gray-200 shadow-md flex items-center justify-center text-xl font-semibold text-gray-800 mx-auto">
+      <div className="w-[636px] bg-gray-200 shadow-md flex items-center justify-center text-xl font-semibold text-gray-800 mx-auto py-3">
         {name}
       </div>
     );
